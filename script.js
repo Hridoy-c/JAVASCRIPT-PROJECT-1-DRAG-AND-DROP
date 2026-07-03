3e74611368c8984461ef8dd3176946c6d53a5705
Hridoy <gazihridoy2006@gmail.com>
2026-07-03 11:25:06 +0600

    again tesing

--- script.js
+++ script.js
@@ -4,28 +4,26 @@ let right = document.getElementById("right");
 
 
 for(list of lists){
-    list.addEventListener("dragstart", function(e){
-        let selected = e.target;
+   list.addEventListener("dragstart", function(e){
+    let section = e.target;
 
-        right.addEventListener("dragover", function(e){
-            e.preventDefault();
-        });
-
-        right.addEventListener("drop", function(e){
-            right.appendChild(selected);
-            selected=null;
-        });
-
-
-
-
-        left.addEventListener("dragover", function(e){
-            e.preventDefault();
-        });
-
-        left.addEventListener("drop", function(e){
-            left.appendChild(selected);
-            selected=null;
-        });
+    right.addEventListener("dragover", function(e){
+        e.preventDefault();
     });
-} 
+
+    right.addEventListener("drop", function(e){
+        right.appendChild(section);
+        section=null;
+    });
+
+    left.addEventListener("dragover", function(e){
+        e.preventDefault();
+    });
+
+    left.addEventListener("drop", function(e){
+        left.appendChild(section);
+        section=null;
+
+    });
+   });
+}
